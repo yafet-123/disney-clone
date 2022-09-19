d19efd36b0d1f2eb46b49a294943447be57906df
yafet-123 <yafetaddisu123@gmail.com>
2022-09-19 13:42:21 +0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,18 @@ import styles from '../styles/Home.module.css'
 import {useEffect} from 'react'
 import ImgSlider from '../components/ImgSlider'
 import Viewes from '../components/Viewes'
-
+import Movies from '../components/Movies'
+import db from '../firebase.js'
+import { collection, getDocs } from "firebase/firestore"
 
 export default function Home() {
+    
+
     return (
         <div className={styles.Container}>
             <ImgSlider />
             <Viewes />
+            <Movies />
         </div>
     )
 }
